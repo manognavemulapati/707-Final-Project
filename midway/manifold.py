1524de7b43eb2092c5475e5b8a891f25d6d3ef14
Manogna Vemulapati <mvemulap@andrew.cmu.edu>
2019-05-02 18:16:49 -0400

    All manifold algorithms

--- midway/manifold.py
+++ midway/manifold.py
@@ -11,14 +11,21 @@ class manifold(object):
         self.n_samples, self.n_features = self.X.shape
         self.n_neighbors = 30
 
-    #Try PCA to see if linear dimensionality reduction works
-    def PCA(self):
-        X_pca = decomposition.TruncatedSVD(n_components=4096).fit_transform(self.X)
-        return X_pca
-
     def Isomap(self):
-        X_iso = manifold.Isomap(n_neighbors, n_components=2).fit_transform(self.X)
+        X_iso = manifold.Isomap(n_neighbors, n_components=4096).fit_transform(self.X)
+        return X_iso
 
     def LLE(self):
-        clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2, method='standard')
+        clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=4096, method='standard')
         X_lle = clf.fit_transform(self.X)
+        return X_lle
+
+    def LTSA(self):
+        clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=4096, method='ltsa')
+        X_ltsa = clf.fit_transform(X)
+        return X_ltsa
+
+    def Spectral(self):
+        embedder = manifold.SpectralEmbedding(n_components=4096, random_state=0, eigen_solver="arpack")
+        X_se = embedder.fit_transform(X)
+        return X_se
